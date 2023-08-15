4d5908f45e48239fc3f8dd261d01ee038e5b0830
Daniyar Slamkul <d.slamkul@aviata.me>
2023-08-15 22:33:41 +0600

    lazycolumn vk news

--- app/src/main/java/com/sumin/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -1,51 +1,28 @@
 package com.sumin.vknewsclient.ui.theme
 
-import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import com.sumin.vknewsclient.MainViewModel
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
-fun MainScreen() {
-
-    val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
-    val fabIsVisible = remember { mutableStateOf(true) }
+fun MainScreen(viewModel: MainViewModel) {
 
     Scaffold(
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
-        },
-        floatingActionButton = {
-            if (fabIsVisible.value) {
-                FloatingActionButton(
-                    onClick = {
-                        scope.launch {
-                            val action = snackbarHostState.showSnackbar(
-                                message = "This is snackbar",
-                                actionLabel = "Hide FAB",
-                                duration = SnackbarDuration.Long
-                            )
-                            if (action == SnackbarResult.ActionPerformed) {
-                                fabIsVisible.value = false
-                            }
-                        }
-                    }
-                ) {
-                    Icon(Icons.Filled.Favorite, contentDescription = null)
-                }
-            }
-        },
         bottomBar = {
             BottomNavigation {
-                Log.d("COMPOSE_TEST", "BottomNavigation")
-
                 val selectedItemPosition = remember {
                     mutableStateOf(0)
                 }
@@ -72,6 +49,40 @@ fun MainScreen() {
             }
         }
     ) {
+        val models = viewModel.feedPosts.observeAsState(listOf())
 
+        LazyColumn(
+            modifier = Modifier.padding(it),
+            contentPadding = PaddingValues(
+                top = 16.dp,
+                bottom = 72.dp,
+                start = 8.dp,
+                end = 8.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(models.value, key = { it.id }) { model ->
+                val dismissState = rememberDismissState()
+
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    viewModel.remove(model)
+                }
+
+                SwipeToDismiss(
+                    modifier = Modifier.animateItemPlacement(),
+                    state = dismissState, background = { },
+                    directions = setOf(DismissDirection.EndToStart)
+                ) {
+                    PostCard(
+                        modifier = Modifier.padding(8.dp),
+                        feedPost = model,
+                        onViewClickListener = { viewModel.updateCount(model, it) },
+                        onShareClickListener = { viewModel.updateCount(model, it) },
+                        onLikeClickListener = { viewModel.updateCount(model, it) },
+                        onCommentClickListener = { viewModel.updateCount(model, it) }
+                    )
+                }
+            }
+        }
     }
 }
